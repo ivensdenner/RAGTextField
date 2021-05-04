ac5ec5da928f4194bcdfb615e6d8e256b84f4a60
Ivens Denner <ivensdenner@gmail.com>
2021-05-04 15:34:42 -0300

    Update iOS Version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "RAGTextField",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v11)
     ],
     products: [
         .library(name: "RAGTextField", targets: ["RAGTextField"])
